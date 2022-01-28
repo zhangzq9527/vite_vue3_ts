118bb975ba49ad9d335fcc73298577f37f1ae2e6
zhangzq9527 <1641376001@qq.com>
2022-01-28 17:11:10 +0800

    study

--- src/router/modules/root.ts
+++ src/router/modules/root.ts
@@ -3,11 +3,6 @@ import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 const routers: RouteRecordRaw[] = [
   {
     path: '/',
-    name: 'index',
-    component: () => import('@/components/HelloWorld.vue'),
-  },
-  {
-    path: '/login',
     name: 'Login',
     component: () => import('@/pages/login/Login.vue'),
   },
