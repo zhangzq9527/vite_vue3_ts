6ecf87a2b4ab5ef6e70e1d404549fd44b4319690
zhangzq9527 <1641376001@qq.com>
2022-01-30 09:44:50 +0800

    study

--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -1,21 +1,74 @@
 <template>
-  <div>用户名:{{ mainStore.name }}<br />长度:{{ mainStore.nameLength }}</div>
-  <hr />
-  <button @click="updateName">修改store中的name</button>
-  <n-button>naiva-ui</n-button>
+  <div>
+    <el-form
+      ref="loginFormRules"
+      :model="loginForm"
+      label-width="80px"
+      class="loginForm"
+      :rules="rules"
+      @keyup.enter="submitLogin()"
+    >
+      <el-form-item label="账号:" prop="username">
+        <el-input v-model="loginForm.username" :prefix-icon="Avatar"></el-input>
+      </el-form-item>
+      <el-form-item label="密码:" prop="password">
+        <el-input v-model="loginForm.password" type="password" :prefix-icon="Lock"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" class="loginbtn" :loading="loading" @click="submitLogin()"
+          >登录</el-button
+        >
+        <router-link to="/Register" style="text-decoration: none">
+          <el-button type="primary">注册</el-button>
+        </router-link>
+      </el-form-item>
+    </el-form>
+  </div>
 </template>
 
-<script setup lang="ts">
-import { useUserStore } from '@/store'
-import { NButton } from 'naive-ui'
+<script lang="ts" setup>
+import { reactive, ref } from 'vue'
+import { Avatar, Lock } from '@element-plus/icons-vue'
+import { ElMessage } from 'element-plus'
 
-const mainStore = useUserStore()
+const loginForm = reactive({
+  username: '',
+  password: '',
+})
+const loginFormRules = ref()
+const rules = reactive({
+  username: [
+    {
+      required: true,
+      message: '请输入账号',
+      trigger: 'blur',
+    },
+  ],
+  password: [
+    {
+      required: true,
+      message: '请输入密码',
+      trigger: 'blur',
+    },
+  ],
+})
+const loading = ref(false)
 
-const updateName = () => {
-  // $patch 修改 store 中的数据
-  mainStore.$patch({
-    name: '名称被修改了,nameLength也随之改变了',
-  })
-  console.log(import.meta.env.VITE_APP_WEB_URL)
+const submitLogin = async () => {
+  loading.value = true
 }
 </script>
+
+<style lang="scss" scoped>
+.loginForm {
+  width: 350px;
+  position: relative;
+  margin: 0 auto;
+  margin-top: 20px;
+}
+
+.loginbtn {
+  margin-left: 50px;
+  margin-right: 40px;
+}
+</style>
